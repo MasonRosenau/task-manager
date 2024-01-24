4c50cc3e061f6bc79a35f249adcd8d09d35dbef8
your-github-username <rosenaumason@gmail.com>
2024-01-23 17:57:21 -0800

    prompt for import functionalized

--- task-manager.c
+++ task-manager.c
@@ -2,6 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**********************************************************************************
+    ** Description: Prompt's the user on whether they would like to import tasks
+    from a file or start fresh with no tasks.
+    ** Parameters: Takes in a buffer, the bufferSize, and a variable to keep track
+    of the number of characters read.
+**********************************************************************************/
+char* promptImport(char* buffer, size_t bufferSize, size_t charsRead)
+{
+    printf("Enter a filename/filepath to load import tasks!\nOtherwise, just hit 'Enter' to start fresh.\n");
+
+    //take in user input; getline can dynamically resize buffer to >32
+    charsRead = getline(&buffer,&bufferSize,stdin);
+
+    /*
+    getline will always return a newline at the end of the input.
+    if the user just hits enter, this line creates
+    if the user enters a file name, this removes the newline
+    */
+    buffer[charsRead - 1] = '\0';
+    
+    return buffer;
+}
+
 int main(int argc, char *argv[])
 {   
     char* buffer;
@@ -10,28 +33,25 @@ int main(int argc, char *argv[])
 
     //create buffer
     buffer = (char *)malloc(bufferSize * sizeof(char));
-    if( buffer == NULL)
+    if(buffer == NULL)
     {
         perror("Unable to allocate buffer");
         exit(1);
     }
-
-    //prompt user
-    printf("Please type a file name to load tasks from files. Otherwise, hit Enter to start fresh: ");
-    charsRead = getline(&buffer,&bufferSize,stdin);
-
     
-    if(buffer[0] == '\n' && charsRead == 1)
+    //prompt user to import tasks or start fresh
+    promptImport(buffer, bufferSize, charsRead);
+
+    //if the first character in buffer exists (isn't just a \0)
+    if(*buffer)
     {
-        printf("You hit enter\n");
+        printf("Opening file '%s'...\n", buffer);
     }
     else
     {
-        //remove last char (newline)
-        buffer[charsRead - 1] = '\0';
-        printf("File name: '%s'\n", buffer);
+        printf("Starting fresh!\n");
     }
+    
     free(buffer);
-
     return 0;
 }
